27a5a5789d1561bb8709548b39eeb7245ac96b51
Jonida Baci <jonida.baci@hotmail.com>
2022-10-20 18:12:39 +0200

    feat: Added api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = `e3893238-414f-4a5b-b84b-4267e34fef8e`;
 export const MODAL_CLOSE_SEC = 2.5;
